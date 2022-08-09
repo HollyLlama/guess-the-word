2d0c9913de7cb30f06748b1a82ec86432dd2274d
Holly Yánez <52582724+HollyLlama@users.noreply.github.com>
2022-08-08 23:02:43 -0400

    add player input validation and input capture function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //unordered list where the player’s guessed letters will appear
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersList = document.querySelector(".guessed-letters");
 //button with the text “Guess!” in it.
 const guessButton = document.querySelector(".guess");
 //text input where the player will guess a letter.
@@ -19,6 +19,8 @@ const playButton = document.querySelector(".play-again");
 
 //starting word to test out the game until you fetch words from a hosted file
 const word = "magnolia";
+//array will contain all the letters the player guesses
+const guessedLetters = [];
 
 // To display dots for each letter of placeholder word
 const placeholder = function (word) { //regular ol' function with a parameter
@@ -28,7 +30,7 @@ const placeholder = function (word) { //regular ol' function with a parameter
     placeholderLetters.push("●"); //for every letter of my word, make a dot and put it into my empty array
   }
   wordProgress.innerText = placeholderLetters.join("");//now change the inner text of my word-in-progess <p> tag to the new array, but join that array so there's no commas between the letters
-}
+};
 
 placeholder(word);//calling my function with an argument (global variable above)
 
@@ -36,7 +38,37 @@ placeholder(word);//calling my function with an argument (global variable above)
 //click event listener for guess button
 guessButton.addEventListener("click", function (e){
   e.preventDefault(); //since this is a form, we want to prevent the default behavior of clicking a button, the form submitting, and then reloading the page.
+  message.innerText = ""; //clear out the message paragraph
   const input = guessInput.value; //grab the gussed input value
-  console.log(input);
-  guessInput.value = " ";//clear the guessed input value to clear the form field
-})
+  const goodGuess = checkPlayerInput(input); //calling validation function (below) and passing input value
+  if (goodGuess) { //run this function only after the input is validated
+    makeGuess(input);
+  }
+  guessInput.value = "";//clear the guessed input value to clear the form field
+});
+
+//check that the player used a letter
+const checkPlayerInput = function(input){ 
+  //this is a regular expression that finds text that matches a specific pattern
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0){ //check if anything has be guessed
+    message.innerText = "Take a guess!";
+  } else if (input.length > 1){ //check if there is more than 1 letter
+    message.innerText = "One letter at a time please.";
+  } else if (!input.match(acceptedLetter)){ //check if a letter was guessed or something else
+    message.innerText = "Letters only please.";
+  } else { //all systems go! we got a guess!
+    return input;
+  }
+};
+
+//function to capture input
+const makeGuess = function (letter) {
+  const letterUp = letter.toUpperCase(); //JavaScript is case sensitive, so let's fix it
+  if (guessedLetters.includes(letterUp)) { //if the user guesses the same letter in the array then output this message
+    message.innerText = "You already guessed that one.";
+  } else {
+    guessedLetters.push(letterUp); // if this is a new letter, put it into my empty array
+  }
+  console.log(guessedLetters);
+};
